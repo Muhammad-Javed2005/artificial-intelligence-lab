deebfea1ecfaebc447d3ff5f2c0e19c9b6bf0dc5
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-08-03 21:12:29 +0500

    Swap Four Variable

--- LAB 01/swap_variables.py
+++ LAB 01/swap_variables.py
@@ -1,3 +1,17 @@
-# Muhammad Javed
+# Lab Task 01
+# Question 1: Swap Four Variables
 
-print("Muhammad Javed")
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+c = int(input("Enter value for c: "))
+d = int(input("Enter value for d: "))
+
+
+print("\Before Swapping")
+print(f"a = {a}, b = {b}, c = {c}, d = {d}")
+
+a , b , c , d = d , c , b , a
+
+
+print("\nAfter Swapping")
+print(f"a = {a}, b = {b}, c = {c}, d = {d}")
