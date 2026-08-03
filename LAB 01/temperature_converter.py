344a15b007ffa8d9d73a13fc6797b15aee1ea83c
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-08-03 21:17:07 +0500

    Temperature Converter

---
+++ LAB 01/temperature_converter.py
@@ -0,0 +1,8 @@
+# Lab Task 01
+# Question 2: Celsius to Fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius}°C is equal to {fahrenheit}°F")
