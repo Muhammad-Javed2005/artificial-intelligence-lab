c855e1d16d5513ca5f120543973dcc585e35b1a3
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-08-24 23:08:23 +0500

    Update Lab File

--- LAB 01/swap_variables.py
+++ LAB 01/swap_variables.py
@@ -7,10 +7,11 @@ c = int(input("Enter value for c: "))
 d = int(input("Enter value for d: "))
 
 
-print("\Before Swapping")
+print("\nBefore Swapping")
 print(f"a = {a}, b = {b}, c = {c}, d = {d}")
 
 a , b , c , d = d , c , b , a
+
 
 
 print("\nAfter Swapping")
