63f273a15626c0abd5ef90b161d0a098f0332b1c
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-09-07 19:44:06 +0500

    Lambda

--- LAB 03/lambda.py
+++ LAB 03/lambda.py
@@ -1,6 +1,7 @@
 import datetime
 
 # Task I: Square and Cube using Lambda
+
 numbers = [1, 2, 3, 4, 5]
 squares = list(map(lambda x: x**2, numbers))
 cubes = list(map(lambda x: x**3, numbers))
@@ -10,12 +11,14 @@ print("Squared List:", squares)
 print("Cubed List:", cubes)
 
 # Task II: String Start-Character Check
+
 starts_with = lambda string, char: string.lower().startswith(char.lower())
 test_str = "Python"
 test_char = "P"
 print(f"Does '{test_str}' start with '{test_char}'? -> {starts_with(test_str, test_char)}")
 
 # Task III: Extract Year, Month, Date, Time
+
 now = datetime.datetime.now()
 get_year = lambda dt: dt.year
 get_month = lambda dt: dt.month
