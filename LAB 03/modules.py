4a937d8d29486961f80a5a551707c4e6efb559e1
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-09-07 19:45:52 +0500

    Modules

--- LAB 03/modules.py
+++ LAB 03/modules.py
@@ -6,21 +6,25 @@ import glob
 import random
 
 # Module Import & Aliasing
+
 print("math.sqrt(9) =", math.sqrt(9))
 print("maths.sqrt(16) =", maths.sqrt(16))
 print("squareroot(25) =", squareroot(25))
 
 # Time Module
+
 current_time = time.time()
 print("\nEpoch Time:", current_time)
 print("Human Readable Time:", time.ctime(current_time))
 time.sleep(1)
 
 # Glob Module
+
 print("\nAll files:", glob.glob("*"))
 print("Python files:", glob.glob("*.py"))
 
 # Random Module
+
 print("\nRandom Int (1-10):", random.randint(1, 10))
 print("Random Float:", random.random())
 x = [1, 2, 3, 4, 5]
